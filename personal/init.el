5507b434fddb8b4cce4fd7993ac18c79e53dc3ae
Curtis Edmond <curtis.edmond@gmail.com>
2020-01-11 13:41:43 -0700

    switch up themes

--- personal/init.el
+++ personal/init.el
@@ -1,6 +1,27 @@
 (disable-theme 'zenburn)
 
-(load-theme 'night-owl t)
+;;(load-theme 'night-owl t)
+
+(require 'doom-themes)
+;; Global settings (defaults)
+(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+
+;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each
+;; theme may have their own settings.
+(load-theme 'doom-one t)
+
+;; Enable flashing mode-line on errors
+(doom-themes-visual-bell-config)
+
+;; Enable custom neotree theme
+(doom-themes-neotree-config)  ; all-the-icons fonts must be installed!
+
+;; Corrects (and improves) org-mode's native fontification.
+(doom-themes-org-config)
+
+(require 'doom-modeline)
+(doom-modeline-mode 1)
 
 (setq whitespace-line-column 120)
 
